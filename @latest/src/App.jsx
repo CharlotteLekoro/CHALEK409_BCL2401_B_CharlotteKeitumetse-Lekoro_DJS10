a8ad915defd6b7e0177b53188e958c7751eaf90c
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-13 16:16:56 +0200

    this is  conditional error, if the error state is not 'null', a <p> element in red. it the error is 'null' it means there is no error and and it will renderthe <ul> which will be the blog posts.

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -21,3 +21,23 @@ function App() {
 
     fetchPosts();
   }, []);
+
+  return (
+    <div>
+      {error ? (
+        <p style={{ color: 'red' }}>{error}</p>
+      ) : (
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
